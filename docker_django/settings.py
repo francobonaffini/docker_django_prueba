19dc5ba058822ab40b39ca1964ed024d161d3d68
Franco Bonaffini <codigofranco1@gmail.com>
2024-05-04 15:59:50 -0300

    fourth commit

--- docker_django/settings.py
+++ docker_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b#bndp8hn5uh82qx8_^oxm923-7szdojlq^=8e^)p+5e969cmn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
